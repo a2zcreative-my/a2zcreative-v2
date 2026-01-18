a5e3bdb127e35ca1218dbf3788cc1f00672094da
M. Alif Farhan <aliffarhan1997@gmail.com>
2026-01-18 17:14:50 +0800

    fix: add edge runtime config to invoice page

--- src/app/invoice/[id]/page.tsx
+++ src/app/invoice/[id]/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const runtime = 'edge';
+
 import { useEffect, useState, useRef, use } from "react";
 import { useRouter } from "next/navigation";
 import { DashboardLayout } from "@/components/layout";
